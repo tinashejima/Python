60baa4b1f9cf10b09cbd78f5c4c805db5fc5a788
tinashejima <jimatinashe@gmail.com>
2025-09-15 22:11:12 +0200

    edit code

--- lab_monitoring_dashboard/app.py
+++ lab_monitoring_dashboard/app.py
@@ -231,6 +231,6 @@ def facility_statuses():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+     app.run(host='0.0.0.0', port=5000, debug=True)
 
 
